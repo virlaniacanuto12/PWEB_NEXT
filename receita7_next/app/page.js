d42c3afaae200b947571413c180d1bac4337d6a9
Virlania Canuto <virlaniacanuto190@gmail.com>
2025-01-19 17:04:40 -0300

    Add estrutura receita 7

---
+++ receita7_next/app/page.js
@@ -0,0 +1,23 @@
+export default function Home(){
+
+    return (
+
+        <div>
+
+            <div>Menu principal</div>
+
+            <div>
+
+                <h1>
+
+                    Viva Santana!
+
+                </h1>
+
+            </div>
+
+        </div>       
+
+    )
+
+}
