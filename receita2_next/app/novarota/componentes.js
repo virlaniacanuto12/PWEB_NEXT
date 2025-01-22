3a43e75c9a140e3744d013f0301e486a286203b9
virlaniacanuto12 <micarlacanuto12@gmail.com>
2025-01-22 17:48:45 -0300

    Atualizando receita2

--- receita2_next/app/novarota/componentes.js
+++ receita2_next/app/novarota/componentes.js
@@ -3,8 +3,9 @@ mas podem ser usadas como marcas em código HTML dentro de outras funções.
 Notem que essas funções podem ter variáveis, if's, for's e fazer o processamento que quiserem, 
 mas precisam retornar um código HTML. As funções exportadas como default também podem ser usadas como marcas em outas funções.*/
 
-export function MariaPrea(){
+export function MariaPrea({ mensagem }) {
     return (
-       <h2>Morreu Maria Preá...</h2>
-    )
-}
+      <h2>{mensagem}</h2>
+    );
+  }
+  
--- receita2_next/app/novarota/page.js
+++ receita2_next/app/novarota/page.js
@@ -5,7 +5,8 @@ export default function NovaRotaHome(){
     return (
        <div>
           <h1>Nova Rota, Nova Página</h1>
-          <MariaPrea/>
+          <MariaPrea mensagem="Morreu Maria Preá..." />
+          <MariaPrea mensagem="Mas voltou para assombrar!" />
        </div>
     )
 }
